0c64f70b0ffcaa0b24717089271af7b4f6ee3b56
sunchaohui <sunchaohui@foxmail.com>
2018-04-03 18:30:33 +0800

    提交代码

---
+++ ijkPlayerDemo/app/src/main/java/com/demo/yilv/videodemo/MainActivity.java
@@ -0,0 +1,102 @@
+package com.demo.yilv.videodemo;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.demo.yilv.videodemo.media.IRenderView;
+import com.demo.yilv.videodemo.media.IjkVideoView;
+import com.demo.yilv.videodemo.media.PlayerManager;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private IjkVideoView mVideoView;
+    private PlayerManager player;
+
+    private String url5 = "http://stream1.grtn.cn/tvs2/sd/live.m3u8?_ts&time=1518428696629";
+    private String url6 = "http://218.207.213.137//PLTV/88888888/224/3221225879/index.m3u8";
+    private String url7 = "http://183.251.61.207/PLTV/88888888/224/3221225829/index.m3u8";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mVideoView = (IjkVideoView) findViewById(R.id.video_view);
+
+
+        /** 普通播放 start **/
+        mVideoView.setAspectRatio(IRenderView.AR_ASPECT_FIT_PARENT);
+        mVideoView.setVideoURI(Uri.parse(url5));
+        mVideoView.start();
+        /** 普通播放 end **/
+
+        //initVideo();
+    }
+
+    //使用滑动控制的话解开注释
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (player.gestureDetector.onTouchEvent(event))
+//            return true;
+//        return super.onTouchEvent(event);
+//    }
+
+    /**
+     * 可左半屏滑动控制亮度  右半屏控制音量  双击切换比例  （无提示）
+     */
+    private void initVideo() {
+        player = new PlayerManager(this);
+        player.setFullScreenOnly(true);
+        player.live(true);
+        player.setScaleType(PlayerManager.SCALETYPE_WRAPCONTENT);
+        player.playInFullScreen(true);
+        player.setPlayerStateListener(new PlayerManager.PlayerStateListener() {
+            @Override
+            public void onComplete() {
+                Log.e("   player  status    :", "complete");
+            }
+
+            @Override
+            public void onError() {
+                Log.e("   player  status    :", "error");
+            }
+
+            @Override
+            public void onLoading() {
+                Log.e("   player  status    :", "loading");
+            }
+
+            @Override
+            public void onPlay() {
+                Log.e("   player  status    :", "play");
+            }
+        });
+        player.play(url5);
+        IjkVideoView videoView = player.getVideoView();
+        videoView.setOnInfoListener(new IMediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(IMediaPlayer iMediaPlayer, int i, int i1) {
+                switch (i) {
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                        break;
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                    case MediaPlayer.MEDIA_INFO_VIDEO_TRACK_LAGGING:
+                        break;
+                }
+                return false;
+
+            }
+        });
+    }
+}
